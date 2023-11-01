77853a2458c6157b646e73a8a044a610d82e277c
Evan Losh <evand.losh@gmail.com>
2023-10-31 20:04:19 -0400

    first deliverable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Log the form input
+  const taskDescription = document.getElementById("create-task-form");
+  taskDescription.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e.target.description.value); // Why did have have to remove hyphens from the name of the form input?
+
+    const li = document.getElementById('tasks');
+    const task = document.createElement("li");
+    task.textContent = e.target.description.value;
+    li.append(task);
+  })
+
+  
+
+  // Add a list element upon submit event
+
 });
