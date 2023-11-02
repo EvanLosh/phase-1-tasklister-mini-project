53b39fca3dde4810809807c112396200b524d525
Nick Sette <njs2394@gmail.com>
2023-11-02 15:34:18 -0400

    Deleting

--- src/index.js
+++ src/index.js
@@ -5,13 +5,39 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     console.log(e.target.description.value); // Why did have have to remove hyphens from the name of the form input?
 
+
+    //Controlling our elements through creation
     const li = document.getElementById('tasks');
+
+    const button = document.createElement('button')
     const task = document.createElement("li");
+
+    button.classList.add("deleteButton")
+
+
     task.textContent = e.target.description.value;
+    button.textContent = 'delete';
+
     li.append(task);
+    li.append(button)
+
+
+
+    button.addEventListener('click', (e) => {
+      task.remove()
+      button.remove()
+    })
+
+
+    // const deleteButton = document.getElementsByClassName('deleteButton')
+
+    // deleteButton[0].addEventListener('click', (e) => {
+    //   console.log(e)
+    // })
+
   })
-  // Completion of deliverable 1 -^
-
-
-
 });
+
+
+
+
